38345fffed1254e46e0cc3f9bd173dea3ef85672
choijaewoong <jwchoi0129@gmail.com>
2015-10-07 17:17:21 +0900

    newInstance

--- app/src/main/java/com/torerov/fragment/OneFragment.java
+++ app/src/main/java/com/torerov/fragment/OneFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +16,46 @@ import android.view.ViewGroup;
  */
 public class OneFragment extends Fragment {
 
+    public static final String EXTRA_MESSAGE = "message";
+    public static OneFragment newInstance(String message){
+        OneFragment f = new OneFragment();
+        Bundle b = new Bundle();
+        b.putString(EXTRA_MESSAGE, message);
+        f.setArguments(b);
+        return f;
+    }
+
 
     public OneFragment() {
         // Required empty public constructor
     }
 
+    TextView messageView;
+    EditText inputView;
+    String message;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle b = getArguments();
+        message = b.getString(EXTRA_MESSAGE);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        messageView = (TextView)view.findViewById(R.id.text_message);
+        messageView.setText(message);
+        inputView = (EditText)view.findViewById(R.id.edit_input);
+        Button btn = (Button)view.findViewById(R.id.btn_send);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageView.setText(inputView.getText().toString());
+            }
+        });
+        return view;
     }
 
 
