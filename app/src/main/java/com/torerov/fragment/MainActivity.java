957dc89466a55f7962ac861a7af46637dae2482e
choijaewoong <jwchoi0129@gmail.com>
2015-10-07 17:17:01 +0900

    newInstance

--- app/src/main/java/com/torerov/fragment/MainActivity.java
+++ app/src/main/java/com/torerov/fragment/MainActivity.java
@@ -9,36 +9,46 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String F1_TAG = "f1_tag";
+    private static final String F2_TAG = "f2_tag";
+    Fragment f1,f2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        f1 = OneFragment.newInstance("hello!!!");
+        f2 = new TwoFragment();
 
-        Button btn = (Button)findViewById(R.id.btn_f1);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment f = new OneFragment();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.container, f);
-                ft.commit();
-            }
-        });
-
-        btn = (Button)findViewById(R.id.btn_f2);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment f = new TwoFragment();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.container, f);
-                ft.commit();
-            }
-        });
         Fragment f = new OneFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.container, f);
+        ft.add(R.id.container, f1, F1_TAG);
         ft.commit();
 
+        Button btn = (Button) findViewById(R.id.btn_f1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = getSupportFragmentManager().findFragmentByTag(F1_TAG);
+                if (f == null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.container, f1, F1_TAG);
+                    ft.commit();
+                }
+            }
+        });
+
+        btn = (Button) findViewById(R.id.btn_f2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = getSupportFragmentManager().findFragmentByTag(F2_TAG);
+                if (f == null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.container, f2, F2_TAG);
+                    ft.commit();
+                }
+            }
+        });
     }
 }
